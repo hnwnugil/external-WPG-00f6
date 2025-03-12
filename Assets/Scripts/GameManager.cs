c31d68e31bb9b78574e3e5a435035de682bafe6f
aditG555 <152684971+aditG555@users.noreply.github.com>
2025-03-12 19:08:37 +0700

    Scene Slide

--- Assets/Scripts/GameManager.cs
+++ Assets/Scripts/GameManager.cs
@@ -1,8 +1,49 @@
+using Unity.VisualScripting;
 using UnityEngine;
 using UnityEngine.UIElements;
 public class GameManager : MonoBehaviour
 {
     [SerializeField] public UIDocument UIDocument;
+    public float SceneSwipeFactor = 900;
+    public static GameManager _instance;
+    //public static GameManager Instance
+    //{
+    //    get
+    //    {
+    //        if (_instance == null)
+    //        {
+    //            _instance = this;
+    //        }
+    //        return _instance;
+    //    }
+    //}
+    public static GameManager GetInstance()
+    {
+        if (_instance == null)
+        {
+            _instance = new GameObject("_GameManager").AddComponent<GameManager>();
+        }
+
+        return _instance;
+    }
+    public static float GetSceneSwipeFactor()
+    {
+        return GetInstance().SceneSwipeFactor;
+    }
+
+
+    private void Awake()
+    {
+        if (_instance == null)
+        {
+            _instance = this;
+        }
+        else if(_instance != this)
+        {
+            Destroy(this);
+        }
+        DontDestroyOnLoad(this);
+    }
     void Start()
     {
         
